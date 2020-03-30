a5c48cfbc64f4f1a05f11c9510ffc647d8bf639f
Pedro Drumond <pedrogsd@al.insper.edu.br>
2020-03-30 11:18:21 -0300

    Come Out pronto

--- Exercício_jogo_de_Craps.py
+++ Exercício_jogo_de_Craps.py
@@ -6,17 +6,19 @@ Created on Wed Mar 25 17:17:57 2020
 """
 
 import random
-jogadaUm=0
+
+jogadaUm = 0
 
 
-def jogo():
-    RespostaSimNao = str(input('Você quer jogar nosso maravilhoso Craps (sim/não)???'))
+def jogo()  :
+    RespostaSimNao = str(input('Você quer jogar nosso maravilhoso Craps (sim/não)??? '))
     RespostaSimNao = RespostaSimNao.lower()
     
     while RespostaSimNao == 'sim':
         dadoUm = random.randint(1,6)
         dadoDois = random.randint(1,6)
         SomaDados= dadoUm+dadoDois
+        print ("Você tirou", SomaDados)
         
         if SomaDados == (2, 3, 12):
             RespostaSimNao = str(input('Você já perdeu?! Quer jogar de novo??? '))
@@ -25,8 +27,12 @@ def jogo():
         elif SomaDados == (7, 11):
             RespostaSimNao = str(input('Você venceu! Não quer tentar a sorte de novo? '))
             RespostaSimNao = RespostaSimNao.lower()
+            
+#19-28 pertencem a jogada "Come Out"
+          
         else:
-            jogadaUm==SomaDados
+            jogadaUm == SomaDados
+            print (RespostaSimNao)
             
 
 
